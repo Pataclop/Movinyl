f2316b608c904d240fd33659fb64eacbb207a72b
flo <escure.f@gmail.com>
2020-04-09 14:28:33 +0200

    setup script, simplified architecture, start of auto retreiving information

---
+++ src/pymdb/pymdb/__init__.py
@@ -0,0 +1,1 @@
+from .pymdb import *
---
+++ src/pymdb/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pymdb',
+      version='0.1',
+      description='Python interface for Omdb API and IMDb Top 250',
+      url='http://github.com/kaushiksk/pymdb',
+      author='Kaushik S Kalmady',
+      author_email='kaushikskalmady@yahoo.in',
+      license='MIT',
+      packages=['pymdb'],
+      zip_safe=False)
