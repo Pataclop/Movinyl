8b3c92d5b1f53ceb1ebc11f973fed9f8c14d737a
flo <escure.f@gmail.com>
2020-04-12 00:28:39 +0200

    basic functionalyties implemented

--- interface.py
+++ interface.py
@@ -5,18 +5,31 @@
 # Created by: PyQt5 UI code generator 5.14.2
 #
 # WARNING! All changes made in this file will be lost!
-
-
+from PyQt5.QtCore import pyqtSignal, Qt, pyqtSlot, QTime
+from PyQt5.QtGui import QColor, QImage, QPixmap
+from PyQt5.QtWidgets import (QHBoxLayout, QVBoxLayout, QLineEdit, QHeaderView,
+                             QMenu, QAction, QAbstractItemView,
+                             QTableWidgetItem, QSizePolicy)
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon, QPixmap
 from tmp import *
+import os
+
+TARGET = "PROCESSING_ZONE"
+color_text = ""  # R   G   B   R   G   B   R   G   B   R   G   B "
+imageNumber = 0
+imageList = os.listdir(TARGET)
+color_list = []
+
+
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(1015, 586)
         self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(550, 210, 311, 281))
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(550, 180, 400, 300))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
         self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
@@ -66,15 +79,32 @@ class Ui_Dialog(object):
         self.l12 = QtWidgets.QLabel(self.gridLayoutWidget)
         self.l12.setObjectName("l12")
         self.gridLayout.addWidget(self.l12, 4, 2, 1, 1)
+
         self.zoom_label = QtWidgets.QLabel(Dialog)
         self.zoom_label.setGeometry(QtCore.QRect(540, 50, 100, 100))
         self.zoom_label.setObjectName("zoom_label")
+
         self.zoom_color_label = QtWidgets.QLabel(Dialog)
         self.zoom_color_label.setGeometry(QtCore.QRect(700, 50, 100, 100))
         self.zoom_color_label.setObjectName("zoom_color_label")
-        self.label_18 = QtWidgets.QLabel(Dialog)
-        self.label_18.setGeometry(QtCore.QRect(70, 520, 391, 31))
-        self.label_18.setObjectName("label_18")
+
+        self.next_label = QtWidgets.QLabel(Dialog)
+        self.next_label.setGeometry(QtCore.QRect(700, 520, 100, 50))
+        self.next_label.setObjectName("next_label")
+
+        self.clearLabel = QtWidgets.QLabel(Dialog)
+        self.clearLabel.setGeometry(QtCore.QRect(475, 520, 30, 30))
+        self.clearLabel.setObjectName("clearLabel")
+
+
+        #self.prev_label = QtWidgets.QLabel(Dialog)
+        #self.prev_label.setGeometry(QtCore.QRect(800, 520, 100, 50))
+        #"self.prev_label.setObjectName("prev_label")
+
+        self.colors_label = QtWidgets.QLabel(Dialog)
+        self.colors_label.setGeometry(QtCore.QRect(70, 520, 390, 31))
+        self.colors_label.setObjectName("colors_label")
+
         self.image_label = QtWidgets.QLabel(Dialog)
         self.image_label.setGeometry(QtCore.QRect(0, 0, 500, 500))
         
@@ -82,34 +112,8 @@ class Ui_Dialog(object):
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, Dialog):
-        result = colorz("image.jpg", 16)
-        color_list=list(result)
 
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.l14.setText(_translate("Dialog", "14"))
-        self.l3.setText(_translate("Dialog", "3"))
-        self.l1.setText(_translate("Dialog", "1"))
-        self.l10.setText(_translate("Dialog", "10"))
-        self.l2.setText(_translate("Dialog", "TextLabel"))
-        self.l7.setText(_translate("Dialog", "TextLabel"))
-        self.l13.setText(_translate("Dialog", "12"))
-        self.l6.setText(_translate("Dialog", "TextLabel"))
-        self.l9.setText(_translate("Dialog", "TextLabel"))
-        self.l4.setText(_translate("Dialog", "TextLabel"))
-        self.l5.setText(_translate("Dialog", "TextLabel"))
-        self.l11.setText(_translate("Dialog", "TextLabel"))
-        self.l8.setText(_translate("Dialog", "18"))
-        self.l15.setText(_translate("Dialog", "15"))
-        self.l12.setText(_translate("Dialog", "12"))
-        self.zoom_label.setText(_translate("Dialog", "TextLabel"))
-        self.zoom_color_label.setText(_translate("Dialog", "TextLabel"))
-        self.label_18.setText(_translate("Dialog", "TextLabel"))
-        self.image_label.setObjectName("image_label")
-        self.image_label.setPixmap(QtGui.QPixmap("test.png"))
-        self.image_label.show() # You were missing this.
-        self.image_label.setScaledContents(True)
+    def setLabelColor(self, color_list):
         self.l1.setStyleSheet("background-color: "+str(color_list[0]))
         self.l2.setStyleSheet("background-color: "+str(color_list[1]))
         self.l3.setStyleSheet("background-color: "+str(color_list[2]))
@@ -125,9 +129,318 @@ class Ui_Dialog(object):
         self.l13.setStyleSheet("background-color: "+str(color_list[12]))
         self.l14.setStyleSheet("background-color: "+str(color_list[13]))
         self.l15.setStyleSheet("background-color: "+str(color_list[14]))
-       
 
+
+    def click1(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[0].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click2(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[1].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click3(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[2].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click4(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[3].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click5(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[4].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click6(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[5].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click7(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[6].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click8(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[7].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click9(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[8].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click10(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[9].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click11(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[10].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click12(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[11].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click13(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[12].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click14(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[13].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+    def click15(self, event):
+        verif="0123456789 "
+        global color_text
+        h=color_list[14].lstrip('#')
+        rgb=tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+        srgb=str(rgb)
+        for c in srgb :
+            a=True
+            for x in verif:
+                if c==x :
+                    a=False
+            if a :
+                srgb=srgb.replace(c,"");
+        color_text = color_text + srgb + " "
+        self.colors_label.setText(color_text)
+
+    def clearFun(self, event):
+        global color_text
+        color_text = ""
+        self.colors_label.setText(color_text)
+
+    def funNext(self, event):
+        global imageNumber
+        global color_list 
+        global color_text
+        imageNumber = imageNumber+1
+        curent_image = TARGET+'/'+imageList[imageNumber]
+        result = colorz(curent_image, 15)
+        color_list = list(result)
+        self.setLabelColor(color_list)
+        self.image_label.setPixmap(QtGui.QPixmap(curent_image))
+        color_text = ""
+        self.colors_label.setText("")
+
+    
+
+    def retranslateUi(self, Dialog):
+        global color_list 
+        curent_image = TARGET+'/'+imageList[imageNumber]
+        result = colorz(curent_image, 15)
+        color_list = list(result)
+
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.l14.setText(_translate("Dialog", "14"))
+        self.l3.setText(_translate("Dialog", "3"))
+        self.l1.setText(_translate("Dialog", "1"))
+        self.l10.setText(_translate("Dialog", "10"))
+        self.l2.setText(_translate("Dialog", "2"))
+        self.l7.setText(_translate("Dialog", "7"))
+        self.l13.setText(_translate("Dialog", "12"))
+        self.l6.setText(_translate("Dialog", "13"))
+        self.l9.setText(_translate("Dialog", "9"))
+        self.l4.setText(_translate("Dialog", "4"))
+        self.l5.setText(_translate("Dialog", "5"))
+        self.l11.setText(_translate("Dialog", "11"))
+        self.l8.setText(_translate("Dialog", "18"))
+        self.l15.setText(_translate("Dialog", "15"))
+        self.l12.setText(_translate("Dialog", "12"))
+
+        self.next_label.setText(_translate("Dialog", "           --->"))
+        self.clearLabel.setText(_translate("Dialog", "X"))
+        #self.prev_label.setText(_translate("Dialog", "<---"))
+
+        self.zoom_label.setText(_translate("Dialog", "TextLabel"))
+        self.zoom_color_label.setText(_translate("Dialog", "TextLabel"))
+        self.colors_label.setText(_translate("Dialog", color_text))
+        self.image_label.setObjectName("image_label")
+        self.colors_label.setTextInteractionFlags(Qt.TextSelectableByMouse)
+
+
+        #self.prev_label.setStyleSheet("background-color: #00B903")
+        self.next_label.setStyleSheet("background-color: #9f9f9f")
+
+        self.image_label.setPixmap(QtGui.QPixmap(curent_image))
+        self.image_label.show() 
+        self.image_label.setScaledContents(True)
         
+        self.setLabelColor(color_list)
+
+        self.l1.mousePressEvent = self.click1
+        self.l2.mousePressEvent = self.click2
+        self.l3.mousePressEvent = self.click3
+        self.l4.mousePressEvent = self.click4
+        self.l5.mousePressEvent = self.click5
+        self.l6.mousePressEvent = self.click6
+        self.l7.mousePressEvent = self.click7
+        self.l8.mousePressEvent = self.click8
+        self.l9.mousePressEvent = self.click9
+        self.l10.mousePressEvent = self.click10
+        self.l11.mousePressEvent = self.click11
+        self.l12.mousePressEvent = self.click12
+        self.l13.mousePressEvent = self.click13
+        self.l14.mousePressEvent = self.click14
+        self.l15.mousePressEvent = self.click15
+        self.next_label.mousePressEvent = self.funNext
+        self.clearLabel.mousePressEvent = self.clearFun
+        
+
+
 
 
         
